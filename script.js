0b8413ed30557ecc23169ed5444e30953214d4ef
Reymond Gulan <reymond@vernaschediewelt.com>
2024-07-31 03:53:08 +0800

    added about section

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const navbar = document.querySelector('.navbar');
 const hamburger = navbar.querySelector('.navbar__hamburger');
 const menu = document.querySelector('.mobile__menu');
 const body = document.querySelector('body');
+const year = new Date().getFullYear();
+const startYear = 2019;
+var totalYears = (year - startYear);
+document.getElementById('total-experience').innerText = totalYears;
 
 function toggleMenu() {
   hamburger.classList.toggle('navbar__hamburger--active');
