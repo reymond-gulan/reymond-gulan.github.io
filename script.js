a5e7bf986148cfb390581b5750cdfe278dbee110
Reymond Gulan <reymond@vernaschediewelt.com>
2024-08-17 05:03:39 +0800

    added work section

--- script.js
+++ script.js
@@ -50,8 +50,59 @@ window.onresize = function (e) {
 //   devTools.appendChild(img);
 // }
 
-document.getElementById('total-experience').innerText = totalYears;
+let global = document.getElementById('dev-tools-global');
+let alpha = document.getElementById('dev-tools-alpha');
+let rsd = document.getElementById('dev-tools-rsd');
 
+let globalContent = alphaContent = rsdContent = "";
+
+const tools = {
+  global: {
+    tools: ['Laravel 9', 'HTML', 'CSS', 'Bootstrap', 'TailwindCSS', 'Javascript', 'jQuery', 'VueJS', 'React', 'MongoDB', 'Postman', 'GitHub', 'BitBucket'],
+    api: ['Shopify', 'Recharge Payments', 'Everstox', 'Brevo', 'Vimeo', 'MailTrap']
+  },
+  alpha: {
+    tools: ['Laravel 8', 'HTML', 'CSS','Bootstrap', 'Javascript', 'jQuery', 'VueJS', 'MySQL', 
+      'Laravel Homestead', 'Laravel Horizon', 'Laravel Forge', 'Laravel Echo', 'GitHub'],
+    api: ['Twilio', 'MailGun', 'MailTrap']
+  },
+  rsd: {
+    tools: ['Laravel 7', 'HTML', 'CSS', 'Bootstrap', 'Javascript', 'jQuery', 'MySQL'],
+    api: []
+  }
+};
+
+
+for (let [key, value] of Object.entries(tools.global.tools)) {
+  globalContent += '<span class="badge">'+ value +'</span> ';
+}
+globalContent += '<br />';
+for (let [key, value] of Object.entries(tools.global.api)) {
+  globalContent += '<span class="badge">'+ value +'</span> ';
+}
+
+for (let [key, value] of Object.entries(tools.alpha.tools)) {
+  alphaContent += '<span class="badge">'+ value +'</span> ';
+}
+alphaContent += '<br />';
+for (let [key, value] of Object.entries(tools.alpha.api)) {
+  alphaContent += '<span class="badge">'+ value +'</span> ';
+}
+
+for (let [key, value] of Object.entries(tools.rsd.tools)) {
+  rsdContent += '<span class="badge">'+ value +'</span> ';
+}
+alphaContent += '<br />';
+for (let [key, value] of Object.entries(tools.rsd.api)) {
+  rsdContent += '<span class="badge">'+ value +'</span> ';
+}
+
+
+global.innerHTML = globalContent;
+alpha.innerHTML = alphaContent;
+rsd.innerHTML = rsdContent;
+
+document.getElementById('total-experience').innerText = totalYears;
 function toggleMenu() {
   hamburger.classList.toggle('navbar__hamburger--active');
   menu.classList.toggle('mobile__menu--active');
